0fcb63360038cc798eaf379b2ac5cc6acb3d7836
Kieran Emery <kizwiz@hotmail.co.uk>
2024-05-05 23:50:16 +0100

    Add project files.

---
+++ VegWizUI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace VegWizUI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
